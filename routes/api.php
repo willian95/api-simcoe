cab25411b0d6a6163582958c7056a258806854f4
Victor Moreno <vamzfe@gmail.com>
2022-01-22 18:47:01 -0400

    [feature/APIADMINSIM-2] - Se corrigio bug en ruta de peticion

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\File\FileController;
 
 /*
 |--------------------------------------------------------------------------
