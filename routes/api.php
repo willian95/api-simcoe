02ec8b641923bec6006a5284cf365fbe6c03f226
willian95 <rodriguezwillian95@gmail.com>
2022-01-21 21:51:30 -0400

    [staging] - Login listo

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\User\UserController;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|   //
 */
 
 
